a1cbc1cc0c50af812b53577800300fd930b54507
tksgtkm <h9cd342fd7jgx36@gmail.com>
2021-10-19 08:41:31 +0900

    make datagram socket program

---
+++ sockets/ud_ucase.h
@@ -0,0 +1,13 @@
+#ifndef __UD_UCASE_H
+#define __UD_UCASE_H
+
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include "api_hdr.h"
+
+#define BUF_SIZE 10
+
+#define SV_SOCK_PATH "/tmp/ud_ucase"
+
+#endif
